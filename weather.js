f98c4fd0daec2888b2d18c77e4bdaa4e8b7959f6
Nobu Matsui <nobuhiro.matsui@gmail.com>
2020-06-30 19:44:02 +0930

    save

--- weather.js
+++ weather.js
@@ -1,33 +1,73 @@
 $("document").ready(function () {
-    console.log("Hello World")
+    console.log("Hello World");
 
-    // var selectedCity = $("#dropDown").val();
+    $("#Submit").on("click", function (event) {
+        event.preventDefault();
+        // Select a option from dropdown menu
+        var teamCity = $("#drop-down option:selected").text().split(" ")[0];
+        console.log(teamCity);
+        
 
 
-    var selectedCity = $('#dropMenu').find(":selected").text();
-    console.log(selectedCity);
+        // condition for london base team
+        if ( (teamCity == "Crystal") || ( teamCity == "West") || ( teamCity == "Arsenal")) {
+            teamCity = "london";
+            console.log(teamCity);
+        }
+
+        // openweather api
+        var weartherQuery = "http://api.openweathermap.org/data/2.5/weather?q=";
+        var apiKey = "d9d510d60f97d35c269584bef800f1b0";
+
+
+        // constructing openweather api url
+        weartherQuery += teamCity + "&appid=" + apiKey;
+
     
 
-    var weartherQuery = "http://api.openweathermap.org/data/2.5/weather?q=";
-    var apiKey = "d9d510d60f97d35c269584bef800f1b0";
-
-    
-    weartherQuery +=  selectedCity + "&appid=" + apiKey;
-    console.log(weartherQuery);
-
-    function callApi (weatherQuery) {
-        $.ajax({
-            url: weatherQuery,
-            method: "GET"
-        }).then(function (response){
-            console.log("test");
-            var selectedWeather = "";
-        });
-    }
+        console.log(weartherQuery);
 
 
+
+        callWeather(weartherQuery);
+        clear();
+
+        // Creating weather infor & Icon api
+        function callWeather(weatherQuery) {
+            $.ajax({
+                url: weatherQuery,
+                method: "GET"
+            }).then(function (response) {
+
+    // displaying icons
+                var weatherIconNum = response.weather[0].icon;
+                var weatherUrl = "http://openweathermap.org/img/wn/" + weatherIconNum + "@2x.png";
+                var weatherIconDisplay = $("<img>").attr("src", weatherUrl);
+                
+    // basic weather info
+                var ul = $("<ul>");
+                var cityli = $("<li>");
+                var humidityli = $("<li>");
+                var templi = $("<li>");
+
+                var cityName = cityli.text("City: " + response.name);
+                var humidity = humidityli.text("Humidity: " + response.main.humidity + "%");
+                var temp = templi.text("Temperature: " + Math.round(response.main.temp - 273.15) + "°C");
+
+                var list = ul.append(cityName, humidity, temp);
+                $("#weather").append(weatherIconDisplay, list );
+
+            });
+        }
+        
+    });
+
+
+// clear weather result
+function clear() {
+    $("#weather").empty();
+}
 
 
 });
-
 
