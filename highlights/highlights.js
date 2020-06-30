6946631f5578df0de24d31c6aeafe915724814b1
Benjamin Turnbull <benorule@outlook.com>
2020-06-30 18:52:53 +0930

    changed API to display only video and not other information below

--- highlights/highlights.js
+++ highlights/highlights.js
@@ -26,23 +26,32 @@ window.onclick = function (event) {
 
 
 
-// URL for scorebat video API
-var queryURL = "https://www.scorebat.com/video-api/v1/";
+// // URL for scorebat video API
+// var queryURL = "https://cors-anywhere.herokuapp.com/https://www.scorebat.com/video-api/v1/";
 
-// Get data from API using URL
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function (result) {
-  console.log(result);
-  console.log(result[26].url);
-  var videoURL = (result[26].url);
+// // Get data from API using URL
+// $.ajax({
+//   url: queryURL,
+//   method: "GET"
+// }).then(function (result) {
+//   // console.log(result);
+//   // console.log(result[26].videos[0].embed);
+//   // var videoURL = result[26].videos[0].embed;
+//   html = $.parseHTML(result[26].videos[0].embed);
+//   console.log(html)
+//   console.log(html[0].find("iframe"))
 
-  var video = document.getElementById("video");
-  var source = document.createElement("source");
+//   var video = $("#modal-content");
 
-  source.setAttribute('src', videoURL);
+  
 
-  video.appendChild(source);
+//   video.append(html.find("iframe"));
 
-});
+// });
+
+
+  var root = $("#root");
+  var str = "<div style='width:100%;height:0px;position:relative;padding-bottom:calc(56.25% + 335px);' class='_scorebatEmbeddedPlayerW_'><iframe src='https:\/\/www.scorebat.com\/embed\/g\/824178\/?s=2' frameborder='0' width='560' height='650' allowfullscreen allow='autoplay; fullscreen' style='width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden;' class='_scorebatEmbeddedPlayer_'><\/iframe><\/div>";
+  var html = $.parseHTML(str);
+  // Append the parsed HTML
+  root.append(html);
